703c350e0363c89ca09bfce6f5e0ceb01f94b7c1
Dézsi Ádám <adam.dezsi@isrv.hu>
2017-02-01 15:40:52 +0100

    dynamic table

--- web-ui/app/scripts/app.js
+++ web-ui/app/scripts/app.js
@@ -2,6 +2,57 @@ angular.module("GameOfLife", [])
     .constant("REST_API", "/api")
     .controller('GameOfLifeController', ['$scope', '$http',
         function ($scope, $http) {
-            $scope.hello = "Adam";
+
+            $scope.state = "stopped";
+            $scope.width = 5;
+            $scope.height = 5;
+
+            $scope.board = getDefaultBoard($scope.width, $scope.height);
+            
+            $scope.startStop = function(){
+                console.log($scope.width + " " + $scope.height);
+            };
+
+            function getDefaultBoard(width, height){
+                var board = [];
+                var column = [];
+                for (var i = 0; i < height ; i++){
+                    column.push(false);
+                }
+                for (var j = 0; j < width; j++){
+                    board.push(angular.copy(column));
+                }
+                return board;
+            }
+
+            $scope.$watch('width', function() {
+                if ($scope.width < 1){
+                    $scope.width = 1;
+                }
+                if ($scope.board.length < $scope.width) {
+                    var column = [];
+                    for (var i = 0; i < $scope.height ; i++){
+                        column.push(false);
+                    }
+                    $scope.board.push( column );
+                } else if ($scope.board.length > $scope.width){
+                    $scope.board.pop();
+                }
+            });
+
+            $scope.$watch('height', function() {
+                if ($scope.height < 1){
+                    $scope.height = 1;
+                }
+                if ($scope.board[0].length < $scope.height) {
+                    for (var i = 0; i < $scope.width ; i++){
+                        $scope.board[i].push(false);
+                    }
+                } else if ($scope.board[0].length > $scope.height){
+                    for (var i = 0; i < $scope.width ; i++){
+                        $scope.board[i].pop();
+                    }
+                }
+            });
         }
     ]);
